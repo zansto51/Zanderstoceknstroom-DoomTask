1efdbd3b6fef0ae6651522fabb15ea2599d58269
zansto51 <88062423+zansto51@users.noreply.github.com>
2022-06-01 13:23:29 +1200

    doing the enemy AI

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -28,8 +28,8 @@ func get_input():
 	
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.y * mouse_sensitivity)
-		$Pivot.rotate_x(-event.relative.x * mouse_sensitivity)
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Pivot.rotation.x = clamp($Pivot.rotation.x,-1.2,1.2) 
 	
 func _physics_process(delta):
