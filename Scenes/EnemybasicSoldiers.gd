6833745d83dd8c84baf99394e6e4b759fc45865e
zansto51 <88062423+zansto51@users.noreply.github.com>
2022-08-05 12:22:13 +1200

    05/08/2022
    doing part 12 part one

--- Scenes/EnemybasicSoldiers.gd
+++ Scenes/EnemybasicSoldiers.gd
@@ -9,6 +9,9 @@ var speed = 6
 var health = 20
 var move = true
 
+var searching = false
+onready var ray = $Visual
+
 func _ready():
 	pass
 	
@@ -24,14 +27,32 @@ func take_damage(dmg_amount):
 	move = true
 	
 func _physics_process(delta):
-	if path_index < path.size():
-		var direction = (path[path_index] - global_transform.origin)
-		if direction.length() < 1:
-			path_index += 1
+	if searching:
+		if path_index < path.size():
+			var direction = (path[path_index] - global_transform.origin)
+			if direction.length() < 1:
+				path_index += 1
+			else:
+				if move:
+					move_and_slide(direction.normalized() * speed, Vector3.UP)
+	else:
+		$AnimatedSprite3D.play("idle")
+		
+func look_at_player():
+	ray.look_at(player.global_transform.origin)
+	if ray.is_colliding():
+		if ray.collider().is_in_group("Player"):
+			searching = true
+			print("I see you")
 		else:
-			if move:
-				move_and_slide(direction.normalized() * speed, Vector3.UP)
-	
+			searching = false
+			var check_near = $Aural.get_overlapping_bodies()
+			for body in check_near:
+				if body.is_in_group("Player"):
+					searching = true
+		
+		
+		
 func find_path(target):
 	path =nav.get_simple_path(global_transform.origin,target)
 	path_index = 0
@@ -52,3 +73,9 @@ func shoot(target):
 
 func _on_Timer_timeout():
 	find_path(player.global_transform.origin)
+
+
+func _on_Aural_body_entered(body):
+	if body.is_in_group("Player"):
+		print("I hear you")
+		searching = true
